b6e157f5c264692f4d9b3d28861072ae7c584d0d
nhadinat <nathanhadinata@gmail.com>
2015-11-29 12:02:05 -0800

    Separate concerns

---
+++ js/collection.js
@@ -0,0 +1,32 @@
+var initialCats = [
+  {
+    clickCount: 0,
+    name: 'Buttons',
+    imgSrc: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+    nicknames: ['Butt-butt', 'Tons', "Buns"]
+  },
+  {
+    clickCount: 0,
+    name: 'Chewie',
+    imgSrc : 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496',
+    nicknames: ['Chewbacca']
+  },
+  {
+    clickCount: 0,
+    name: 'Pumpkin',
+    imgSrc : 'http://s3.amazonaws.com/readers/2012/01/25/320pxredcat8727_1.jpg',
+    nicknames: ['Pumps']
+  },
+  {
+    clickCount: 0,
+    name: 'Metoo',
+    imgSrc : 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/snowshoe-cat3.jpg',
+    nicknames: ['U2']
+  },
+  {
+    clickCount: 0,
+    name: 'Tootsie',
+    imgSrc : 'http://4hdwallpapers.com/wp-content/uploads/2013/04/Funny-Little-Brown-Cat-1024x768.jpg',
+    nicknames: ['Toot-toot']
+  }
+];
