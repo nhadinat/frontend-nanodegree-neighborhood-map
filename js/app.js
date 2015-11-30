b7696ec82ade5cd631a243b06b8bdf28646587bb
nhadinat <nathanhadinata@gmail.com>
2015-11-29 16:28:35 -0800

    Add markers

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+var map;
+function initMap() {
+  var lollicup = {lat: 33.6879372, lng:-117.8341776};
+  var parkingStructure = {lat: 33.688131,  lng:-117.834479};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 33.688428, lng: -117.833741},
+    zoom: 18
+  });
+
+  var markerLollicup = new google.maps.Marker({
+    position: lollicup,
+    map: map,
+    title: 'Lollicup'
+  });
+
+  var markerParkingStructure = new google.maps.Marker({
+    position: parkingStructure,
+    map: map,
+    title: 'DJ Parking Structure'
+  });
+}
+
+/* Cat reference
 var ViewModel = function () {
   var self = this;
 
@@ -20,5 +44,6 @@ var ViewModel = function () {
 };
 
 ko.applyBindings(new ViewModel());
+*/
 
 //Google Map API key: AIzaSyCFRFOiufsaKBMx3jmskWF1KEiEwZCudcs
