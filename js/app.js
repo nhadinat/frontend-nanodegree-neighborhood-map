477869b527a8ab91f740cf66b92d263d4f17e98c
nhadinat <nathanhadinata@gmail.com>
2015-12-05 09:21:17 -0800

    Update 2015-12-05T17:21:13.974Z

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
+var map;
+function initMap() {
+  var lollicup = {lat: 33.6879372, lng:-117.8341776};
+  var tokyoTable = {lat: 33.687387, lng:-117.833752};
+  var theKickingCrab = {lat: 33.688507, lng:-117.832728};
+  var balconyGrillBar = {lat: 33.687824, lng:-117.834221};
+  var ajisenRamen = {lat: 33.688856, lng:-117.833937};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 33.688428, lng: -117.833741},
+    zoom: 18
+  });
+
+  var markerLollicup = new google.maps.Marker({
+    position: lollicup,
+    map: map,
+    title: 'Lollicup'
+  });
+
+  var markerBalconyGrillBar = new google.maps.Marker({
+    position: balconyGrillBar,
+    map: map,
+    title: 'Balcony Grill & Bar'
+  });
+
+  var markerTokyoTable = new google.maps.Marker({
+    position: tokyoTable,
+    map: map,
+    title: 'Tokyo Table'
+  });
+
+  var markerTheKickinCrab = new google.maps.Marker({
+    position: theKickingCrab,
+    map: map,
+    title: 'The Kicking Crab'
+  });
+
+  var markerAjisenRamen = new google.maps.Marker({
+    position: ajisenRamen,
+    map: map,
+    title: 'Ajisen Ramen'
+  });
+}
+
+/* Cat reference
 var ViewModel = function () {
   var self = this;
 
@@ -20,5 +65,6 @@ var ViewModel = function () {
 };
 
 ko.applyBindings(new ViewModel());
+*/
 
 //Google Map API key: AIzaSyCFRFOiufsaKBMx3jmskWF1KEiEwZCudcs
