818d0fbd2a1be0bb9c4a3b4f9ab1be9b20eb24af
nhadinat <nathanhadinata@gmail.com>
2015-12-13 12:21:23 -0800

    Knockout Library, more model dev

--- js/model.js
+++ js/model.js
@@ -1,6 +1,10 @@
-// Make a Location Class
+// Model: Location Class
 var Location = function (data) {
   var self = this;
+
+  // Name of location and coordinates
+  this.title = ko.observable(data.name);
+  this.latLng = ko.observable(data.latLng);
 
   // For each Location in the collection, create markers as observables
   this.marker = new google.maps.Marker({
