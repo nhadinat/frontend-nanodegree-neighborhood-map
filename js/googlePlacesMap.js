66739ea8fae1af62130f86ffc4959b069ce1f77a
nhadinat <nathanhadinata@gmail.com>
2015-12-19 14:14:10 -0800

    Testing google map samples

--- js/googlePlacesMap.js
+++ js/googlePlacesMap.js
@@ -1,38 +1,30 @@
-var google;
-function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665, 151.1956);
+var map;
+var service;
+var infowindow;
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: pyrmont,
-    zoom: 15,
-    scrollwheel: false
+function initialize() {
+  var lollicup = new google.maps.LatLng(33.6879372, -117.8341776);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: lollicup,
+    zoom: 15
   });
 
-  // Specify location, radius and place types for your Places API search.
   var request = {
-    location: pyrmont,
+    location: lollicup,
     radius: '500',
-    types: ['store']
+    query: 'restaurant'
   };
 
-  // Create the PlaceService and send the request.
-  // Handle the callback with an anonymous function.
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, function(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        var place = results[i];
-        // If the request succeeds, draw the place location on
-        // the map as a marker, and register an event to handle a
-        // click on the marker.
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-      }
-    }
-  });
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
 }
 
-// Run the initialize function when the window has finished loading.
-google.maps.event.addDomListener(window, 'load', initialize);
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
