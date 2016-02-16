67c5acc7171697c739e7b36c049ea284fd34e0ed
Nathan Hadinata <nathanhadinata@gmail.com>
2016-02-15 21:58:50 -0800

    Update 2016-02-16T05:58:47.666Z

--- js/googlePlacesMap.js
+++ js/googlePlacesMap.js
@@ -22,76 +22,17 @@ function initialize() {
     radius: 250,
     types: ['restaurant']
   }, callback);
-
-
-  // This example adds a search box to a map, using the Google Place Autocomplete
-  // feature. People can enter geographical searches. The search box will return a
-  // pick list containing a mix of places and predicted search terms.
-
-  // HOWEVER, for my purpose, I will want to have this filter a select list > collection.js
-
-  // Create the search box and link it to the UI element.
-  var input = /** @type {HTMLInputElement} */(document.getElementById('pac-input'));
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  // Style input to blend with the rest of the google map controls
-  input.style.margin = "10px 0 0 0";
-  input.style.height = "29px";
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // [START region_getplaces]
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-  // [END region_getplaces]
 }
 
+
+
+
+
+
+// MARKER AND INFOWINDOW //
+
 // Create markers based on the PlacesService search data collection
+// This is where the markers will bind to the list collection
 function callback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
@@ -109,7 +50,7 @@ function createMarker(place) {
     position: placeLoc
   });
 
-  // Infowindow on click
+  // Infowindow on click. I can edit the infowindow here. I can add marker buttonBounce here v^.
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent(place.name + ', ♥: ' + place.rating + '/5');
     infowindow.open(map, this);
