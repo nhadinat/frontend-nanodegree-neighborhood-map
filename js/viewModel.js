a47d884749d73c5a031ec66dbe3128205b973049
Nathan Hadinata <nathanhadinata@gmail.com>
2016-02-23 22:03:47 -0800

    Update 2016-02-24T06:03:46.026Z

--- js/viewModel.js
+++ js/viewModel.js
@@ -19,7 +19,35 @@ var ViewModel = function () {
   };
 
   // Detect textInput from view
-  self.filter = ko.observable(""); // We have a connection here, now what?
+  self.filter = ko.observable("");
+  // We have a connection here, now what? indexOf(filter), toLowerCase()
+  self.filterChange = function () {
+      console.log('ey filterChange');
+    if (self.filter() !== "") {
+      console.log('ey filterValue');
+    }
+  };
+  /* COULD THIS BE THE ANSWERS!??!?!?
+  var viewModel = {
+    // …
+
+    search: function(value) {
+      // remove all the current beers, which removes them from the view
+      viewModel.beers.removeAll();
+
+      for(var x in beers) {
+        if(beers[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+          viewModel.beers.push(beers[x]);
+        }
+      }
+    }
+  };
+
+  viewModel.query.subscribe(viewModel.search);
+  */
+
+  // Show Hide List
+  self.showList = ko.observable(true);
 
   // Set current results as the first item
   //self.currentResults = ko.observable(self.list()[0]);
